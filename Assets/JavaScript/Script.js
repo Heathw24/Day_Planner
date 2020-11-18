8ca0524c95c1737a7daabd2f5f92304fa0164c08
Heath Ward <heathw24@outlook.com>
2020-11-18 17:36:34 -0500

    got current time in appropriate format

--- Assets/JavaScript/Script.js
+++ Assets/JavaScript/Script.js
@@ -1,6 +1,11 @@
 var dateEl = document.getElementById("currentDay");
 var schedule = document.querySelector("container")
-var date = moment().format('dddd');
+var date1 = moment().format('dddd');
+var date2 = moment().format('MMM Do');
+date = date1 + " " + date2;
 dateEl.innerHTML = date;
 
 console.log(date)
+
+var hour = moment().format('H');
+console.log(hour)
