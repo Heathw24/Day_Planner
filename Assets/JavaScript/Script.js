5449221ee65b25430cf5d5c5af4b3c0ff76ebb4e
Heath Ward <heathw24@outlook.com>
2020-11-18 18:46:46 -0500

    wokred on timecheck logic

--- Assets/JavaScript/Script.js
+++ Assets/JavaScript/Script.js
@@ -9,3 +9,35 @@ console.log(date)
 
 var hour = moment().format('H');
 console.log(hour)
+
+
+var time0 = $("9");
+var tenAm = $("10");
+var elevenAm = $("11");
+var twelvePm = $("12");
+var onePm = $("13");
+var twoPm = $("14");
+var threePm = $("15");
+var fourPm = $("16");
+var fivePm = $("17");
+
+console.log(fivePm)
+
+for (i = 0; i>9; i++) {
+    var blocktime = i + 9;
+    
+ if (blocktime < hour) {
+        // classList.add("past");
+ }
+
+ if (blocktime == hour) {
+        // classList.add("present");
+ }
+
+ if (blocktime > hour) {
+        //  classList.add("future");
+ }
+
+
+    
+}
