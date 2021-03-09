25ea74035dce0730bcc387a034ad4d6ee97221f2
Heath Ward <heathw24@outlook.com>
2021-03-09 00:04:32 -0500

    set up react for timeBlocks and disabled malfunctioning Script.js page

---
+++ dayplanner/src/App.js
@@ -0,0 +1,78 @@
+// import logo from './logo.svg';
+import './App.css';
+// import './Script'
+import React from "react";
+
+
+function App() {
+  return (
+    <div className="App">
+    
+    <header className="jumbotron">
+      <h1 className="display-3">Work Day Scheduler</h1>
+      <p className="lead">A simple calendar app for scheduling your work day</p>
+      <p id="currentDay" className="lead"></p>
+    </header>
+
+    <div className="container" id = "timeBlocks">
+      
+{/* 
+      <div id="9am" className="row"> 
+        <div className = "hour"><p className="schTime"> 9:00 AM</p></div>
+        <div className = "time-block" id="9"><textarea id="9input" className="0txt"></textarea> </div>
+        <div className = "saveBtn"><button className="buttons btn" id="9amBtn"><i className="fa fa-folder"></i></button></div>
+      </div>
+
+      <div id="10am" className="row"> 
+        <div className = "hour"><p className="schTime"> 10:00 AM</p></div>
+        <div className = "time-block" id="10"><textarea id="10input" className="1txt"></textarea> </div>
+        <div className = "saveBtn"><button className="buttons btn" id="10amBtn"><i className="fa fa-folder"></i></button></div>
+      </div>
+
+      <div id="11am" className="row"> 
+        <div className = "hour"><p className="schTime"> 11:00 AM</p></div>
+        <div className = "time-block" id="11"><textarea id="11input" className="2txt"></textarea> </div>
+        <div className = "saveBtn"><button className="buttons btn" id="11amBtn"><i className="fa fa-folder" ></i></button></div>
+      </div>
+
+      <div id="12pm" className="row"> 
+        <div className = "hour"><p className="schTime"> 12:00 PM</p></div>
+        <div className = "time-block" id="12"><textarea id="12input" className="3txt"></textarea> </div>
+        <div className = "saveBtn"><button className="buttons btn" id="12pmBtn"><i className="fa fa-folder"></i></button></div>
+      </div>
+
+      <div id="1pm" className="row"> 
+        <div className = "hour"><p className="schTime"> 1:00 PM</p></div>
+        <div className = "time-block" id="13"><textarea id="1input" className="4txt"></textarea> </div>
+        <div className = "saveBtn"><button className="buttons btn" id="1pmBtn"><i className="fa fa-folder"></i></button></div>
+      </div>
+
+      <div id="2pm" className="row"> 
+        <div className = "hour"><p className="schTime"> 2:00 PM</p></div>
+        <div className = "time-block" id="14"><textarea id="2input" className="5txt"></textarea> </div>
+        <div className = "saveBtn"><button className="buttons btn" id="2pmBtn"><i className="fa fa-folder"></i></button></div>
+      </div>
+
+      <div id="3pm" className="row"> 
+        <div className = "hour"><p className="schTime"> 3:00 PM</p></div>
+        <div className = "time-block" id="15"><textarea id="3input" className="6txt"></textarea> </div>
+        <div className = "saveBtn"><button className="buttons btn" id="3pmBtn"><i className="fa fa-folder"></i></button></div>
+      </div>
+
+      <div id="4pm" className="row"> 
+        <div className = "hour"><p className="schTime"> 4:00 PM</p></div>
+        <div className = "time-block" id="16"><textarea id="4input" className="7txt"></textarea> </div>
+        <div className = "saveBtn"><button className="buttons btn" id="4pmBtn"><i className="fa fa-folder"></i></button></div>
+      </div>
+      <div id="5pm" className="row"> 
+        <div className = "hour"><p className="schTime" > 5:00 PM</p></div>
+        <div className = "time-block" id="17oclock"><textarea id="5input" className="8txt"></textarea> </div>
+        <div className = "saveBtn"><button className="buttons btn" id="5pmBtn"><i className="fa fa-folder"></i></button></div>
+      </div> */}
+      
+    </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ dayplanner/src/timeblock.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Tblocks() {
+    return (
+    <p> This is a timeblock</p>
+    );
+}
+
+
+export default Tblocks;
+
